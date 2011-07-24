e43f0249b9be7e161bdd5b90fa74076862ab2e2e
David Walter Seikel <won_fang@yahoo.com.au>
2011-07-25 09:57:17 +1000

    A bit of cleaning of the mesh cmake stuff.
    Gonna make a macro or something soon to make it easy to add all those other plugins.
    There's alot of them.

--- linden/indra/cmake/mimesh.cmake
+++ linden/indra/cmake/mimesh.cmake
@@ -7,16 +7,25 @@ set(G3D_INCLUDE_DIRS
 if (LINUX)
     # In order to support using ld.gold on linux, we need to explicitely
     # specify all libraries that this library uses.
-    set(G3D_LIBRARIES g3d magic gsf-1)
+    set(G3D_LIBRARIES
+	g3d
+	magic
+	gsf-1
+	)
 else (LINUX)
-    set(G3D_LIBRARIES g3d)
+    set(G3D_LIBRARIES
+	g3d
+	)
 endif (LINUX)
 
 
 set(MIMESH_INCLUDE_DIRS
-    ${LIBS_OPEN_DIR}/mimesh/libg3d-0.0.8/include
+    ${G3D_INCLUDE_DIRS}
     ${LIBS_OPEN_DIR}/mimesh
     )
 
-set(MIMESH_LIBRARIES mimesh ${G3D_LIBRARIES})
+set(MIMESH_LIBRARIES
+    mimesh
+    ${G3D_LIBRARIES}
+    )
 
